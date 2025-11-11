f10de3c87e75d8a64af4f78f4f15d5f7e1b6b613
Asaf Flescher <asaf.flescher@datadoghq.com>
2025-11-11 15:42:19 -0500

    Fix

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install protols
 
 # Install neovim
-apt-get install -y neovim
+sudo apt-get install -y neovim
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim --headless '+PlugInstall --sync' +qall
 
